4b53ebe4dbd64cc06b795d92fee3c392100d06c5
pierregobin <pierregobinjoomla@free.fr>
2016-11-10 22:21:47 +0100

    add argument passing

--- web.ml
+++ web.ml
@@ -1,7 +1,22 @@
 open Nethttp_client;;
 open Soup;;
 
-let a = Convenience.http_get "http://www.champignonsen3clics.com/top-100-des-champignons-d-automne-partie-1-a-i/";; 
+let a = Convenience.http_get Sys.argv.(1);;
 let a' = Soup.parse a;;
 let w = a' $$ "a[data-href]" |> to_list |> List.map (fun a -> (a |> R.attribute "data-href", a |> R.attribute "data-title") );;
-w |> List.map (fun (url,name) -> (name ^ "\n") |> print_string );;
+w |> List.map (fun (url,name) -> 
+                let jpg =  Str.split (Str.regexp "/") url 
+                           |> Array.of_list 
+                           |> fun x -> Array.get x ((Array.length x) - 2) 
+                in
+                begin
+                        (* Printf.printf "%s -> %s.jpg\n" name jpg; *)
+                        let latin = List.hd (Str.split (Str.regexp " -") name) in
+                        let latin = Str.global_replace (Str.regexp " ") "_" latin in
+                        let c = "wget " ^ url ^ " -O " ^ latin ^ ".jpg" in
+                        begin
+                        Printf.printf "download [%s]\n" name ;
+                        Sys.command(c);
+                        end
+                end
+);;
